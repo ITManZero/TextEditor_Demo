a42932b2ac7a76f86f1e2e5e7518aeff18acadad
ITMManZero <baker.mesl.bm@gmail.com>
2021-08-10 16:14:57 +0300

    5

--- src/main/java/core/model/Document.java
+++ src/main/java/core/model/Document.java
@@ -3,12 +3,11 @@ package core.model;
 import java.util.List;
 
 public class Document {
-    //import
-    //import
-    //import
+    int r;
     int x;
     int y;
     int z;
+    boolean solved;
     //private List<TextRowObject> rows;
 }
 
