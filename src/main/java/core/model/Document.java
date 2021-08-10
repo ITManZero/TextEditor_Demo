80ff2d2d278b114ddf8c95f40e44c820e3b8cc21
ITMManZero <baker.mesl.bm@gmail.com>
2021-08-10 15:48:40 +0300

    kkk

--- src/main/java/core/model/Document.java
+++ src/main/java/core/model/Document.java
@@ -2,6 +2,14 @@ package core.model;
 
 import java.util.List;
 
+class DD {
+    int x;
+    int y;
+    int z;
+    private List<TextRowObject> rows;
+}
+
+
 public class Document {
 
     private List<TextRowObject> rows;
